d642738fa13001560e70a8a078cbbab05cc8ef69
LeiteSS <estoudente.program@gmail.com>
2022-09-06 09:22:00 -0300

    Add comment in main

--- src/main/java/com/fatec/grupo3/Grupo3Application.java
+++ src/main/java/com/fatec/grupo3/Grupo3Application.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Grupo3Application {
-
+	
+	/* 
+	 * AUTHOR: Silas Santos Leite
+	 * Description: Comentando para commitar no Github usando o Spring Tool Suite 4
+	 * Date: Tue, September 6 2022
+	 * */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo3Application.class, args);
 	}
